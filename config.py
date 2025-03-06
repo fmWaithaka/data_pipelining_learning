b6b23f909d473fbf98d8492896fc462c0451a7d0
fmWaithaka <waithaka.francis20@students.dkut.ac.ke>
2025-03-06 13:26:41 +0300

    Etl airflow logic.

--- config.py
+++ config.py
@@ -4,14 +4,16 @@ DB_DETAILS = {
     'dev': {
         'RETAIL_DB': {
             'DB_TYPE': 'mysql',
-            'DB_HOST': 'host.docker.internal',
+            # 'DB_HOST': 'host.docker.internal',
+            'DB_HOST': '127.0.0.1',
             'DB_NAME': 'retail_db',
             'DB_USER': os.environ.get('RETAIL_DB_USER'),
             'DB_PASS': os.environ.get('RETAIL_DB_PASS')
         },
         'CUSTOMER': {
             'DB_TYPE': 'postgres',
-            'DB_HOST': 'host.docker.internal',
+            # 'DB_HOST': 'host.docker.internal',
+            'DB_HOST': '127.0.0.1',
             'DB_NAME': 'retail_db',
             'DB_USER': os.environ.get('CUSTOMER_DB_USER'),
             'DB_PASS': os.environ.get('CUSTOMER_DB_PASS')
